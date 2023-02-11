43227070a66374290a02df4c90aa13362a770112
anne rubio <rubio.anne@gmail.com>
2023-02-11 19:10:05 +0100

    ended css

---
+++ assets/app.js
@@ -0,0 +1,6 @@
+const notifNumber = document.querySelector(".menu-notif")
+const markAllRead = document.querySelector(".menu-status")
+
+  markAllRead.addEventListener("click", (event) => {
+    
+  })
