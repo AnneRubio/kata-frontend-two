d80314da67067c01c7eea95d032c36cf33a0b456
anne rubio <rubio.anne@gmail.com>
2023-02-12 21:56:25 +0100

    added first js event to change text

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,7 @@
-const notifNumber = document.querySelector(".menu-notif")
-const markAllRead = document.querySelector(".menu-status")
+const markAllRead = document.querySelector(".menu-status");
+const notifCount = document.querySelector(".menu-notif");
 
-  markAllRead.addEventListener("click", (event) => {
-    
-  })
+// listening an event on Mark All Read to on click change text of notifs from 3 to 0
+markAllRead.addEventListener("click", function() {
+  notifCount.innerText = "0";
+});
