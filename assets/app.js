b878b400f269012014ec432d34819c15996a6919
anne rubio <rubio.anne@gmail.com>
2023-02-13 22:08:57 +0100

    added new js event to remove notif dots

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,11 @@
 const markAllRead = document.querySelector(".menu-status");
 const notifCount = document.querySelector(".menu-notif");
 
-// listening an event on Mark All Read to on click change text of notifs from 3 to 0
+// listening an event on Mark All Read so on click, text from the ul menu changes from 3 to 0
 markAllRead.addEventListener("click", function() {
   notifCount.innerText = "0";
+  let notifDot = document.querySelectorAll(".dot");
+  notifDot.forEach((dot) => {
+    dot.classList.remove("dot");
+  });
 });
