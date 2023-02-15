93ae1f7b1a9c02128eae10c4b683bd19c50c9813
anne rubio <rubio.anne@gmail.com>
2023-02-15 22:11:51 +0100

    added js event to change style on click

--- assets/app.js
+++ assets/app.js
@@ -1,11 +1,21 @@
+// listening an event on Mark All Read so on click:
+//  text from the ul menu changes from 3 to 0
+//  notif dots disappear from the web page
+//  lightblue background color from unread notifs changes to white = as read notice
 const markAllRead = document.querySelector(".menu-status");
 const notifCount = document.querySelector(".menu-notif");
-
-// listening an event on Mark All Read so on click, text from the ul menu changes from 3 to 0
 markAllRead.addEventListener("click", function() {
   notifCount.innerText = "0";
   let notifDot = document.querySelectorAll(".dot");
   notifDot.forEach((dot) => {
     dot.classList.remove("dot");
   });
+  let nonReadNotif = document.querySelectorAll(".unread-notif");
+  nonReadNotif.forEach((unreadNotif) => {
+    unreadNotif.style.backgroundColor = "white";
+  });
 });
+
+
+let counter = 0;
+
