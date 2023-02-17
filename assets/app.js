ce89b4eadfbbfd51a94776c93d6b4c2510e31d78
anne rubio <rubio.anne@gmail.com>
2023-02-17 16:18:03 +0100

    trying to sort out solution for a next js event

--- assets/app.js
+++ assets/app.js
@@ -1,21 +1,33 @@
+let markAllRead = document.querySelector(".menu-status");
+let notifCount = document.querySelector(".menu-notif");
+
 // listening an event on Mark All Read so on click:
-//  text from the ul menu changes from 3 to 0
-//  notif dots disappear from the web page
-//  lightblue background color from unread notifs changes to white = as read notice
-const markAllRead = document.querySelector(".menu-status");
-const notifCount = document.querySelector(".menu-notif");
 markAllRead.addEventListener("click", function() {
+  //  text from the ul menu changes from 3 to 0
   notifCount.innerText = "0";
+  //  notif dots disappear from the web page
   let notifDot = document.querySelectorAll(".dot");
   notifDot.forEach((dot) => {
     dot.classList.remove("dot");
   });
+  //  lightblue background color from unread notifs changes to white = as read notice
   let nonReadNotif = document.querySelectorAll(".unread-notif");
   nonReadNotif.forEach((unreadNotif) => {
     unreadNotif.style.backgroundColor = "white";
   });
 });
 
+let markRead = document.querySelectorAll(".markRead");
+markRead.forEach(function(element) {
+  element.addEventListener("click", function() {
+  let unreadNotif = document.querySelectorAll(".unread-notif");
+    unreadNotif.forEach((unreadNotif) => {
+    if (unreadNotif.style.backgroundColor != "white")  {
+      unreadNotif.style.backgroundColor = "white";
+      }
+    });
+  });
+});
 
-let counter = 0;
-
+//  listening an event on active names (markRead) so on click:
+// menu-notif -=1 if > 0 && lightblue backround color switches to white, if backrground == blue
