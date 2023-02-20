df6d8ba201de820e583c90b380dd0ab2b62d218d
anne rubio <rubio.anne@gmail.com>
2023-02-20 22:47:09 +0100

    refactoring code with custom functions

--- assets/app.js
+++ assets/app.js
@@ -2,40 +2,48 @@ let markAllRead = document.querySelector(".menu-status");
 let notifCount = document.querySelector(".menu-notif");
 let markAsRead = document.querySelectorAll(".markRead");
 
-// listening an event on Mark All Read so on click:
-markAllRead.addEventListener("click", function() {
-  //  text from the ul menu changes from 3 to 0
-  notifCount.innerText = "0";
-  //  notif dots disappear from the web page
+// creating a function to remove dots from the web page
+function removeDots() {
   let notifDot = document.querySelectorAll(".dot");
   notifDot.forEach((dot) => {
     dot.classList.remove("dot");
   });
-  //  lightblue background color from unread notifs changes to white = as read notice
-  let nonReadNotif = document.querySelectorAll(".unread-notif");
-  nonReadNotif.forEach((unreadNotif) => {
-    unreadNotif.style.backgroundColor = "white";
-  });
-});
+};
 
-// faire une fonction pour que le background soit enlevé 1 par 1 et non pour chaque item. A corriger ici
-function removeOneBackground (e) {
+// creating a function to switch background to white if current background is blue
+function removeOneBackground() {
   let nonReadNotif = document.querySelectorAll(".unread-notif");
   nonReadNotif.forEach((nonReadNotif) => {
+    if (nonReadNotif.style.backgroundColor != "white");
     nonReadNotif.style.backgroundColor = "white";
-    if (this.style.backgroundColor != "white");
   });
 };
 
+// listening an event on Mark All Read so on click:
+markAllRead.addEventListener("click", function() {
+  //  text from the ul menu changes from 3 to 0
+  notifCount.innerText = "0";
+  removeDots();
+  removeOneBackground();
+});
+
+// listening an event on Mark As Read DOM elements so on click background turns white and Dot disappear
+markAsRead.forEach((markRead) => markRead.addEventListener("click", removeDots)); 
 markAsRead.forEach((markRead) => markRead.addEventListener("click", removeOneBackground));
-
-  // let unreadNotif = document.querySelectorAll(".unread-notif");
-  //   unreadNotif.forEach((unreadNotif) => {
-  //   if (unreadNotif.style.backgroundColor != "white")  {
-  //     unreadNotif.style.backgroundColor = "white";
-  //     }
-  //   });
-
 
 //  listening an event on active names (markRead) so on click:
 // menu-notif -=1 if > 0 && lightblue backround color switches to white, if backrground == blue
+// markAllRead.addEventListener("click", function() {
+//   //  text from the ul menu changes from 3 to 0
+//   notifCount.innerText = "0";
+//   //  notif dots disappear from the web page
+//   let notifDot = document.querySelectorAll(".dot");
+//   notifDot.forEach((dot) => {
+//     dot.classList.remove("dot");
+//   });
+//   //  lightblue background color from unread notifs changes to white = as read notice
+//   let nonReadNotif = document.querySelectorAll(".unread-notif");
+//   nonReadNotif.forEach((unreadNotif) => {
+//     unreadNotif.style.backgroundColor = "white";
+//   });
+// });
